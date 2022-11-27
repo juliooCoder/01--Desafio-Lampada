fd8327b9b7f81bf2066bd93b7b927a6395b32491
juliofernandesG <julioocoder@gmail.com>
2022-11-27 16:05:54 -0300

    Desafio da Lâmpada
    Aqui deixo uma maneira simples de liga e desligar e quebrar a lâmpada

---
+++ script.js
@@ -0,0 +1,24 @@
+const turnOn = document.getElementById('turnOn');
+const turnOff = document.getElementById('turnOff');
+const lamp = document.getElementById('lamp');
+
+function lampOn (){
+    lamp.src = './img/ligada.jpg';
+}
+
+function lampOff (){
+    lamp.src = './img/desligada.jpg'
+}
+
+function lampBroken (){
+    lamp.src = './img/quebrada.jpg'
+};
+
+turnOn.addEventListener('click', lampOn);
+turnOff.addEventListener('click', lampOff);
+lamp.addEventListener('mouseover', lampOn);
+lamp.addEventListener('mouseleave', lampOff);
+
+lamp.addEventListener('dblclick', lampBroken);
+
+
